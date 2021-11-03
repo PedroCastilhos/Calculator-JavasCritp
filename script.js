08877530b7dc3314bb7c424a025faed11f8804df
PedroCastilhos <qscdurant8>
2021-11-03 19:57:25 -0300

    Done

--- script.js
+++ script.js
@@ -1,105 +1,27 @@
-var input = document.getElementsByTagName('input')[0];
+function insert(num) {
+  let number = document.getElementById('screen').innerHTML;
+  document.getElementById('screen').innerHTML = number + num;
+}
 
-const zero = document.querySelector('.zero');
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
-const four = document.querySelector('.four');
-const five = document.querySelector('.five');
-const six = document.querySelector('.six');
-const seven = document.querySelector('.seven');
-const eight = document.querySelector('.eight');
-const nine = document.querySelector('.nine');
+function clean() {
+  // A palavra "Clear" é uma palavra reservada do JavaScript
+  document.querySelector('#screen').innerHTML = '';
+}
 
-const point = document.querySelector('.point');
-const plus = document.querySelector('.plus');
-const less = document.querySelector('.less');
-const times = document.querySelector('.times');
-const divisor = document.querySelector('.divisor');
-const equal = document.querySelector('.equal');
+function back() {
+  let result = document.getElementById('screen').innerHTML;
+  document.getElementById('screen').innerHTML = result.substring(
+    0,
+    result.length - 1
+  );
+}
 
-let valor = Number(input.value);
+function calculate() {
+  let result = document.getElementById('screen').innerHTML;
 
-zero.addEventListener('click', function () {
-  valor = 0;
-  input.value += valor;
-  console.log(valor);
-});
-
-one.addEventListener('click', function () {
-  valor = 1;
-  input.value += valor;
-  console.log(valor);
-});
-
-two.addEventListener('click', function () {
-  valor = 2;
-  input.value += valor;
-  console.log(valor);
-});
-
-three.addEventListener('click', function () {
-  valor = 3;
-  input.value += valor;
-  console.log(valor);
-});
-
-four.addEventListener('click', function () {
-  valor = 4;
-  input.value += valor;
-  console.log(valor);
-});
-
-five.addEventListener('click', function () {
-  valor = 5;
-  input.value += valor;
-  console.log(valor);
-});
-
-six.addEventListener('click', function () {
-  valor = 6;
-  input.value += valor;
-  console.log(valor);
-});
-
-seven.addEventListener('click', function () {
-  valor = 7;
-  input.value += valor;
-  console.log(valor);
-});
-
-eight.addEventListener('click', function () {
-  valor = 8;
-  input.value += valor;
-  console.log(valor);
-});
-
-nine.addEventListener('click', function () {
-  valor = 9;
-  input.value += valor;
-  console.log(valor);
-});
-
-plus.addEventListener('click', function () {
-  valor = '+';
-  input.value += valor;
-  console.log(valor);
-});
-
-less.addEventListener('click', function () {
-  valor = '-';
-  input.value += valor;
-  console.log(valor);
-});
-
-divisor.addEventListener('click', function () {
-  valor = '/';
-  input.value += valor;
-  console.log(valor);
-});
-
-times.addEventListener('click', function () {
-  valor = 'x';
-  input.value += valor;
-  console.log(valor);
-});
+  if (result) {
+    document.getElementById('screen').innerHTML = eval(result); // eval é uma função do JS que realiza conta automáticamente
+  } else {
+    alert('Insira algum dado antes de calcular');
+  }
+}
